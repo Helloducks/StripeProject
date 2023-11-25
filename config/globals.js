08e038a3880053f38c75c91d008ad21fb3deb7ea
Saarath Rathee <105334170+Helloducks@users.noreply.github.com>
2023-11-25 17:28:52 -0500

    Update globals.js

--- config/globals.js
+++ config/globals.js
@@ -1,4 +1,4 @@
 const configurations = {
-    'db' : 'mongodb+srv://saarathrathee:qKgZUqqGZ6haDJDr@cluster0.zr2epel.mongodb.net/TestImageUploads'    
+    'db' : '{link:to mongoDB}'    
 }
 module.exports = configurations;
